af17a6779971792d26283ddbdd42db5e8f011c1b
user.name "Eisa Khan <user.email "khanarafat1376@gmail.com>
2025-04-22 18:42:14 +0530

    feat: add RestaurantScreen to navigation stack

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from 'Screens/HomeScreen';
+import RestaurantScreen from 'Screens/RestaurantScreen';
 
 const Stack = createNativeStackNavigator()
 
@@ -13,6 +14,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name='home' component={HomeScreen}/>
+          <Stack.Screen name='Restaurant' component={RestaurantScreen}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
